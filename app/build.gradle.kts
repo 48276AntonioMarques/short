e3f2249701e954290b56efe575676660bc44586a
António Marques <a48276@alunos.isel.pt>
2024-02-27 23:26:05 +0000

    Dependencies and start app
    Added framework dependency
    Setting up main and configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(project(":framework"))
 }
 
 tasks.test {
---
+++ app/src/main/kotlin/pt/isel/tictactoe/TicTacToeApp.kt
@@ -0,0 +1,12 @@
+package pt.isel.tictactoe
+
+import pt.isel.SHORT.Application
+import pt.isel.SHORT.runSHORT
+
+class TicTacToeApp : Application {
+
+}
+
+fun main(args: Array<String>) {
+    runSHORT<TicTacToeApp>(args)
+}
