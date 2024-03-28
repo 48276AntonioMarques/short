412acf087251e78fc403cef115b3da771f179716
António Marques <a48276@alunos.isel.pt>
2024-03-28 05:00:08 +0000

    [Web App] Create Components
    Created Button Component
    Created Leaderboard Component

---
+++ app/src/main/kotlin/pt/isel/tictactoe/components/Button.kt
@@ -0,0 +1,15 @@
+package pt.isel.tictactoe.components
+
+import pt.isel.SHORT.html.Attribute
+import pt.isel.SHORT.html.HtmlTag
+import pt.isel.SHORT.html.Text
+import pt.isel.SHORT.html.element.Div
+import pt.isel.SHORT.html.id
+
+fun HtmlTag.Button(text: String) = apply {
+    Div(
+        attributes = Attribute.id("button")
+    ) {
+        Text(text)
+    }
+}
--- app/src/main/kotlin/pt/isel/tictactoe/pages/Home.kt
+++ app/src/main/kotlin/pt/isel/tictactoe/pages/Home.kt
@@ -1,13 +1,33 @@
 package pt.isel.tictactoe.pages
 
 import pt.isel.SHORT.Page
-import pt.isel.SHORT.component.Var
+import pt.isel.SHORT.html.Attribute
 import pt.isel.SHORT.html.Text
+import pt.isel.SHORT.html.element.Div
 import pt.isel.SHORT.html.element.single.Div
+import pt.isel.SHORT.html.id
+import pt.isel.tictactoe.components.Button
+import pt.isel.tictactoe.components.Leaderboard
 
 @Page("/")
 fun home() =
-    Div {
-        val name = Var("World")
-        Text { "Hello, World!" }
+    Div(
+        attributes = Attribute.id("container")
+    ) {
+        Div(
+            attributes = Attribute.id("window")
+        ) {
+            Div(
+                attributes = Attribute.id("title")
+            ) {
+                Text("Tic Tac Toe")
+            }
+            Div(
+                attributes = Attribute.id("menu")
+            ) {
+                Button("Play")
+                Button("Login")
+            }
+        }
+        Leaderboard()
     }
