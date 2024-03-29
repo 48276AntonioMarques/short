b059b0f49a4ae3a35d9b6d9e23531dc78557e7cc
António Marques <a48276@alunos.isel.pt>
2024-03-29 19:58:40 +0000

    New Framework Stage System
    Implementing a stage system that allows to create routes for static content defined on the web app

--- framework/src/main/kotlin/pt/isel/SHORT/Application.kt
+++ framework/src/main/kotlin/pt/isel/SHORT/Application.kt
@@ -1,16 +1,12 @@
 package pt.isel.SHORT
 
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.server.Jetty
 import org.http4k.server.ServerConfig
+import pt.isel.SHORT.html.HtmlTag
 
-typealias LoadingScreen = String
 open class Application {
 
-    fun getServerConfig(): ServerConfig = Jetty(9000)
+    open fun getServerConfig(): ServerConfig = Jetty(9000)
 
-    fun getLoadingScreen(request: Request): Response =
-        Response(Status.NOT_FOUND).body("Page not found.")
+    open fun getLoadingScreen(root: HtmlTag): HtmlTag? = null
 }
--- framework/src/main/kotlin/pt/isel/SHORT/Initializer.kt
+++ framework/src/main/kotlin/pt/isel/SHORT/Initializer.kt
@@ -8,8 +8,10 @@ import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Http4kServer
 import org.http4k.server.asServer
+import pt.isel.SHORT.html.Html
 import pt.isel.SHORT.html.HtmlPage
-import java.util.concurrent.CompletableFuture
+import pt.isel.SHORT.html.HtmlTag
+import pt.isel.SHORT.html.element.Body
 
 typealias WebApp = HtmlPage
 
@@ -27,6 +29,7 @@ inline fun <reified T : Application> runSHORT(args: Array<String>) {
  * The instance can have more function that will be used to inject dependencies in the pages
  */
 fun <T : Application> runSHORT(sources: Class<T>, args: Array<String>): Http4kServer {
+    // Instantiate the sources manager
     val serverSources: T
 
     try {
@@ -35,25 +38,42 @@ fun <T : Application> runSHORT(sources: Class<T>, args: Array<String>): Http4kSe
         throw RuntimeException("Couldn't create an instance of the server sources", e)
     }
 
-    val webApp = CompletableFuture<WebApp>()
-
-    // TODO: Allow to add more routes
-    val exposedPaths = routes(
+    // Register loading page
+    val loadingPath = routes(
         "/" bind Method.GET to { request: Request ->
-            // Maybe this should be a 404 or a static loading page
-            // Until the page aggregator is finished
-            if (webApp.isDone) {
-                Response(Status.OK).body(webApp.get().toHtml())
+            var loadingScreen: HtmlTag? = null
+            val root = Html {
+                Body {
+                    loadingScreen = serverSources.getLoadingScreen(this)
+                }
+            }
+            if (loadingScreen == null) {
+                Response(Status(102, "Processing")).body("Server is starting.")
             } else {
-                serverSources.getLoadingScreen(request)
+                Response(Status.OK).body(root.toHtml())
             }
         }
     )
 
+    // Launch temporary server
+    val tempServer = loadingPath.asServer(serverSources.getServerConfig()).start()
+
+    // Generate the web app
+    val webApp = generateWebApp()
+
+    // Register new routes
+    val exposedPaths = routes(
+        "/" bind Method.GET to { request: Request ->
+            Response(Status.OK).body(webApp.toHtml())
+        }
+    )
+
+    // Stop the temporary server
+    tempServer.stop()
+
+    // Launch definitive server
     val server = exposedPaths.asServer(serverSources.getServerConfig()).start()
     println("Server started at port ${server.port()}.")
-
-    webApp.complete(generateWebApp())
 
     return server
 }
