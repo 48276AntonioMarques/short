5c1ea326f4cf667231d6a0e272eedeef0e1cdba0
António Marques <a48276@alunos.isel.pt>
2024-04-09 18:51:56 +0100

    [Web App] Adding loading screen

--- app/src/main/kotlin/pt/isel/tictactoe/LoadingScreen.kt
+++ app/src/main/kotlin/pt/isel/tictactoe/LoadingScreen.kt
@@ -1,11 +1,16 @@
 package pt.isel.tictactoe
 
+import pt.isel.SHORT.html.Attribute
 import pt.isel.SHORT.html.Tag
+import pt.isel.SHORT.html.attribute.id
 import pt.isel.SHORT.html.element.Div
 import pt.isel.SHORT.html.element.Img
+import pt.isel.SHORT.html.element.Style
 
 fun Tag.loadingScreen() =
-    Div {
-        // Style("loading.css")
-        Img("loading.gif", embed = true)
+    Div(
+        attributes = Attribute.id("loading-screen")
+    ) {
+        Style("/css/loading.css")
+        Img("/loading.gif", embed = true)
     }
