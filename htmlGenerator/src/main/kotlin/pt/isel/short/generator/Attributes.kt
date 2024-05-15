54107690fde40ada02d15d2dc86cb218fd064438
António Marques <a48276@alunos.isel.pt>
2024-05-15 16:04:46 +0100

    [Generator] Fixing bug that duplicated tables
    Fixing a bug that would duplicate tables which would make all the files be generated twice with the same contents

--- htmlGenerator/src/main/kotlin/pt/isel/short/generator/Attributes.kt
+++ htmlGenerator/src/main/kotlin/pt/isel/short/generator/Attributes.kt
@@ -1,3 +1,101 @@
 package pt.isel.short.generator
 
+import org.http4k.client.ApacheClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+
+const val ATTRIBUTES_SOURCE = "https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes"
 const val ATTRIBUTES_OUTPUT = "./../framework/src/main/kotlin/pt/isel/SHORT/html/attribute/"
+
+fun getAttributes(source: String, blacklist: List<String>): List<Attribute> {
+    val client = ApacheClient()
+    val request = Request(Method.GET, source)
+    val response = client(request)
+    val bodyString = response.bodyString()
+    val safeBodyString = clearScripts(bodyString)
+
+    val tables: List<String> = getTables(safeBodyString)
+    val attributes: List<Attribute> = getAttributesFromTables(tables)
+    val allowedAttributes: List<Attribute> = attributes.filter { (name, _) -> !blacklist.contains(name) }
+    return allowedAttributes
+}
+
+fun getAttributesFromTables(tables: List<String>): List<Attribute> {
+    return tables.flatMap { table ->
+        val tableHead = getTags("thead", table)
+        if (tableHead.size != 1) return@flatMap emptyList()
+        val headRow = getTags("tr", tableHead[0])
+        if (headRow.size != 1) return@flatMap emptyList()
+        val head = getTags("th", headRow[0])
+        if (head.size != 3 || head[0] == "Attribute Name" || head[1] == "Elements" || head[2] == "Description") return@flatMap emptyList()
+
+        val tableBody = getTags("tbody", table)
+        if (tableBody.size != 1) return@flatMap emptyList()
+
+        val rows = getTags("tr", tableBody[0])
+        rows.flatMap { row ->
+            val columns = getTags("td", row)
+            if (columns.size != 3) {
+                emptyList()
+            } else {
+                val codes = getTags("code", columns[0])
+                codes.mapNotNull { code ->
+                    val link = getTags("a", code)
+                    val linkContent = if (link.isEmpty()) {
+                        if (code.contains(Regex(""))) code else ""
+                    } else {
+                        link[0]
+                    }
+                    if (linkContent.isBlank()) return@mapNotNull null
+                    val name = linkContent
+                        .replace(Regex("<[^>]*>"), "")
+                        .replace(Regex("&[^;]*;"), "")
+
+                    val elements = columns[1]
+                        .replace(Regex("<[^>]*>"), "")
+                        .replace(Regex("&[^;]*;"), "")
+                        .split(", ")
+
+                    val descriptionContent = columns[2]
+                        .replace(Regex("<[^>]*>"), "")
+                        .replace(Regex("&[^;]*;"), "")
+                    Attribute(name, elements, descriptionContent)
+                }
+            }
+        }
+    }
+}
+
+fun generateAttributes(attributes: List<Attribute>): List<Pair<String, String>> {
+    // TODO: Format description
+    return attributes.map { (name, elements, description) ->
+        val capitalizedAttribute = name.replaceFirstChar { c -> c.uppercaseChar() }
+        Pair(
+            capitalizedAttribute,
+            """
+                package pt.isel.SHORT.html.attribute
+                
+                import pt.isel.SHORT.html.Attribute
+                import pt.isel.SHORT.html.add
+
+                /**
+                 * Represents the HTML $name attribute.
+                 * Description: ${formatText(description)}
+                 * Supported elements: ${formatText(elements.map { "<$it>" }.joinToString(", "))}
+                 */
+                fun Attribute.Companion.$name(value: String) = add("$name", value)
+                fun List<Attribute>.$name(value: String) = add("$name", value)
+            """.trimIndent()
+        )
+    }
+}
+
+fun formatText(text: String): String {
+    var output = text
+        .replace("\n", " ")
+        .replace("\t", "")
+    while (output.contains("  ")) {
+        output = output.replace("  ", " ")
+    }
+    return output
+}
--- htmlGenerator/src/main/kotlin/pt/isel/short/generator/Elements.kt
+++ htmlGenerator/src/main/kotlin/pt/isel/short/generator/Elements.kt
@@ -9,19 +9,54 @@ const val ELEMENTS_OUTPUT = "./../framework/src/main/kotlin/pt/isel/SHORT/html/e
 
 fun getElements(source: String, blacklist: List<String>): List<Pair<String, String>> {
     val client = ApacheClient()
-    val request = Request(Method.GET, ELEMENTS_SOURCE)
+    val request = Request(Method.GET, source)
     val response = client(request)
     val bodyString = response.bodyString()
+    val safeBodyString = clearScripts(bodyString)
 
-    val tables: List<String> = getTables(bodyString)
-    val elements: List<Pair<String, String>> = getTableRows(tables)
+    val tables: List<String> = getTables(safeBodyString)
+    val elements: List<Pair<String, String>> = getElementsFromTables(tables)
     val allowedElements: List<Pair<String, String>> = elements.filter { (tag, _) -> !blacklist.contains(tag) }
-    // elements.forEach { (tag, description) -> println("$tag -> $description") }
     return allowedElements
 }
 
+fun getElementsFromTables(tables: List<String>): List<Pair<String, String>> {
+    return tables.flatMap { table ->
+        val tableHead = getTags("thead", table)
+        if (tableHead.size != 1) return@flatMap emptyList()
+        val headRow = getTags("tr", tableHead[0])
+        if (headRow.size != 1) return@flatMap emptyList()
+        val head = getTags("th", headRow[0])
+        if (head.size != 2 || head[0] == "Element" || head[1] == "Description") return@flatMap emptyList()
+
+        val tableBody = getTags("tbody", table)
+        if (tableBody.size != 1) return@flatMap emptyList()
+
+        val rows = getTags("tr", tableBody[0])
+
+        rows.flatMap { row ->
+            val columns = getTags("td", row)
+            if (columns.size != 2) {
+                emptyList()
+            } else {
+                val links = getTags("a", columns[0])
+                links.mapNotNull { link ->
+                    val code = getTags("code", link)
+                    if (code.size != 1) return@mapNotNull null
+                    val codeContent = code[0]
+                        .replace(Regex("<[^>]*>"), "")
+                        .replace(Regex("&[^;]*;"), "")
+                    val descriptionContent = columns[1]
+                        .replace(Regex("<[^>]*>"), "")
+                        .replace(Regex("&[^;]*;"), "")
+                    Pair(codeContent, descriptionContent)
+                }
+            }
+        }
+    }
+}
+
 fun generateElements(elements: List<Pair<String, String>>): List<Pair<String, String>> {
-    // TODO: Format description
     return elements.map { (element, description) ->
         val capitalizedElement = element.replaceFirstChar { c -> c.uppercaseChar() }
         Pair(
