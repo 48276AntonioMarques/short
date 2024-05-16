72b1b3cb5cfa2aef85977b9d5b4f62767458af71
António Marques <a48276@alunos.isel.pt>
2024-05-16 15:48:49 +0100

    [App] Fixing loading screen
    Stylesheets now works on loading screen

--- app/src/main/kotlin/pt/isel/tictactoe/LoadingScreen.kt
+++ app/src/main/kotlin/pt/isel/tictactoe/LoadingScreen.kt
@@ -6,12 +6,45 @@ import pt.isel.SHORT.html.attribute.id
 import pt.isel.SHORT.html.attribute.src
 import pt.isel.SHORT.html.element.Div
 import pt.isel.SHORT.html.element.Img
+import pt.isel.SHORT.html.element.Script
 import pt.isel.SHORT.html.element.Style
+import pt.isel.SHORT.html.element.Text
 
 fun Tag.loadingScreen() =
     Div(
         attributes = Attribute.id("loading-screen")
     ) {
-        Style(attributes = Attribute.src("styles/loading.css"))
+        Style {
+            Text {
+                """
+                body {
+                    background-color: #1C2C3A;
+                    overflow: hidden;
+                }
+                #loading-screen {
+                    position: fixed;
+                    z-index: 1000;
+                    top: 45%;
+                    left: 45%;
+                    width: 10%;
+                    height: 10%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                img {
+                    width: 20vh;
+                    height: 20vh;
+                }
+                @media (orientation: portrait) {
+                  img {
+                    width: 20vw;
+                    height: 20vw    ;
+                  }
+                }
+                """.trimIndent()
+            }
+        }
         Img(attributes = Attribute.src("images/loading.gif"))
+        Script(Attribute.src("js/loadingScreen.js"))
     }
