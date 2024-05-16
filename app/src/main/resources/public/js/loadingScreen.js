6942dfc9d027f07ab4f2ec127967d6c15ab2dc93
António Marques <a48276@alunos.isel.pt>
2024-05-16 19:35:16 +0100

    [App] Added a script to load the app when ready
    Added a script to the loading screen that reloads the page when the server is ready to deliver the app

---
+++ app/src/main/resources/public/js/loadingScreen.js
@@ -0,0 +1,16 @@
+const page = `<html><body>${document.body.innerHTML}</body></html>`
+
+async function checkLoadingState() {
+    console.log("Checking loading state...")
+    const response = await fetch("http://localhost:9000")
+    const text = await response.text()
+    if (text != page) {
+        // document.documentElement.innerHTML = page
+        location.reload()
+    }
+    else {
+        setTimeout(checkLoadingState, 500)
+    }
+}
+
+checkLoadingState()
